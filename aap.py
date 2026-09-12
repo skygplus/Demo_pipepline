579a78e65c43282844aeb7054befe35313234846
John Salubi <90048786+skygplus@users.noreply.github.com>
2026-09-12 23:38:17 +0100

    Create aap.py

---
+++ aap.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "success",
+        "message": "Hello from your Jenkins CI/CD Flask App!"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+if __name__ == "__main__":
+    # Important for Docker: listen on all interfaces
+    app.run(host="0.0.0.0", port=5000, debug=False)
